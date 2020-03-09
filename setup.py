9e827792ad5d318e8dfa8dc21c854263fe90df40
prasannals <prasanna.pessu@gmail.com>
2020-03-08 20:23:46 -0400

    Added requirements.txt and install_requires

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ package_data = {
 }
 
 setup(name='tf_object_detection_util',
-      version='0.1.7',
+      version='0.1.8',
       description='A library to make Object Detection using Tensorflow Object Detection API easier.',
       url='https://github.com/prasannals/tf_object_detection_util',
       author='Prasanna Lakkur Subramanyam',
@@ -13,4 +13,13 @@ setup(name='tf_object_detection_util',
       license='MIT',
       packages=find_packages(),
       include_package_data=True,
+      install_requires=[
+            'pandas',
+            'numpy',
+            'six',
+            'tensorflow-gpu==1.9.0',
+            'matplotlib',
+            'opencv_python>=4.1.2.30',
+            'Pillow>=7.0.0'
+      ],
       zip_safe=False)
